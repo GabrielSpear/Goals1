cf42fe83579510fb4cef4f68a4993f4f6a0be56b
Gabriel <gabrieldvjspear@gmail.com>
2018-03-15 22:17:26 +0300

    .

---
+++ Documents/leap-year/js/scripts.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+    $("form#leap-year").submit(function(event) {
+      event.preventDefault();
+      var year = parseInt($("input#year").val());
+      var result = leapYear(year);
+      $("#result").text(result);
+    });
+  });
+
+var leapYear = function(year) {
+      return false;
+    };
+
+var leapYear = function(year) {
+      if (year % 4 === 0) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+
+var leapYear = function(year) {
+        if (year % 100 === 0) {
+          return false;
+        } else if (year % 4 === 0) {
+          return true;
+        } else {
+          return false;
+        }
+      };
+
+      var leapYear = function(year) {
+          if ((year % 4 === 0) && (year % 100 !== 0)) {
+            return true;
+          } else {
+            return false;
+          }
+        };
+
+        var leapYear = function(year) {
+            if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+              return true;
+            } else {
+              return false;
+            }
+          };
